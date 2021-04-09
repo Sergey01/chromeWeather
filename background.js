733ad11e969c18b2edd03c99e3ffd99b6ed4668a
hwpanda <hwpanda225@gmail.com>
2021-04-08 19:39:07 -0700

    The foundation has been laid

---
+++ background.js
@@ -0,0 +1,2 @@
+/* Although the extension has been installed, it has no instruction. Introduce a background script by creating a file titled background.jsand placing it inside the extension directory.
+Background scripts and many other components must be registered in the manifest. Registering a background script in the manifest tells the extension which files to reference, and how that file will behave. */
---
+++ main.js
@@ -0,0 +1,32 @@
+//write something here
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+
+const API_KEY = '0c186a8c759453e0fd31c9939838eb69';
+
+const sendZipCode = function () {
+	let zip = document.getElementById('userInput').value;
+	if (zip.length !== 5) return;
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?zip=${zip},US&units=imperial&appid=${API_KEY}`
+	)
+		.then(function (response) {
+			// check response status and if it's anything but 200, blame the user for entering
+			// an invalid zip code.
+			if (response.status !== 200) {
+				document.getElementById('userInput').value = 'INVALID';
+				return;
+			}
+			console.log(response.status);
+			console.log(response.statusText);
+			return response.json();
+		})
+		.then(function (data) {
+			let conditions = data.weather[0].main;
+			let temp = data.main.temp;
+			document.getElementById('temperature').innerText = 'Temperature: ' + temp;
+			document.getElementById('conditions').innerText =
+				'Conditions: ' + conditions;
+			document.getElementById('zipCode').innerText = 'Zip Code: ' + zip;
+		})
+		.catch((err) => console.log('Something is wrong: ', err)); // end of fetch
+};
